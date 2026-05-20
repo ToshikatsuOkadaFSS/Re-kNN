b6844a4e1290ef47634f6ba939deabf463488080
Toshikatsu Okada <tokada@da2.so-net.ne.jp>
2026-05-21 07:48:39 +0900

    BertClusterin test

---
+++ RekNNDocClutering/Program.cs
@@ -0,0 +1,236 @@
+﻿using FuutaSystemSvcCommonLibrary;
+using RekNNUtility;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+using System.Security.AccessControl;
+using RekNNDocClutering;
+
+const int BERT_VECTOR_SIZE = 768;
+
+
+RekNNDocClutering.CommonValues.LoadSettings();
+
+if (!Directory.Exists(RekNNDocClutering.CommonValues.Settings.DatabasePath))
+{
+    Directory.CreateDirectory(RekNNDocClutering.CommonValues.Settings.DatabasePath);
+}
+
+
+RekNNUtility.RekNNUtility utility = new RekNNUtility.RekNNUtility(ModeEnum.BERT, 1);
+
+utility.LoadModel(0, RekNNDocClutering.CommonValues.Settings.DatabasePath);
+
+DateTime lapStartTime = DateTime.Now;
+
+while (true)
+{
+    Console.WriteLine("1 ... Vectorize (with skip added files), max 10000 files.");
+    Console.WriteLine("2 ... Refine Database (BULK) & Save");
+    Console.WriteLine("99 ... Exit");
+    Console.WriteLine($"Input Command No:");
+
+    if (Console.ReadLine() is string cmd)
+    {
+        if (int.TryParse(cmd, out int v))
+        {
+            if (v == 99)
+            {
+                break;
+            }
+
+            switch (v)
+            {
+                case 1:
+                    ScanAndVectorizeTextAndUpdateDatabase(utility, 1000);
+                    break;
+
+                case 2:
+                    utility.RefineDatabase(0, -1);
+                    utility.Save(0, RekNNDocClutering.CommonValues.Settings.DatabasePath);
+                    RekNNDocClutering.CommonValues.SaveSettings();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Command No");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Command No");
+        }
+    }
+}
+
+RekNNDocClutering.CommonValues.SaveSettings();
+
+
+////////////////////////////////////////////
+/// ここから先は関数定義
+/// 
+
+
+void ScanAndVectorizeTextAndUpdateDatabase(RekNNUtility.RekNNUtility utility, int maxDocuments)
+{
+    if (Directory.Exists(RekNNDocClutering.CommonValues.Settings.TextFilePath) == false)
+    {
+        Console.WriteLine("Text file root path not found");
+        return;
+    }
+
+    if (Directory.Exists(RekNNDocClutering.CommonValues.Settings.VectorFilePath) == false)
+    {
+        Console.WriteLine("Vector file root path not found");
+        return;
+    }
+
+    lapStartTime = DateTime.Now;
+
+    ScanAndVectorizeTextAndUpdateDatabaseFolder(
+        utility, 
+        RekNNDocClutering.CommonValues.Settings.TextFilePath,
+        RekNNDocClutering.CommonValues.Settings.VectorFilePath,
+        maxDocuments,
+        0);
+
+    utility.Save(0, RekNNDocClutering.CommonValues.Settings.DatabasePath);
+    RekNNDocClutering.CommonValues.SaveSettings();
+}
+
+int ScanAndVectorizeTextAndUpdateDatabaseFolder(
+    RekNNUtility.RekNNUtility utility, 
+    string filePath,
+    string vectorPath, 
+    int maxDocuments,
+    int count)
+{
+    DirectoryInfo directoryInfoFile = new DirectoryInfo(filePath);
+    DirectoryInfo directoryInfoVector = new DirectoryInfo(filePath);
+
+    foreach (DirectoryInfo directoryInfo1 in directoryInfoVector.GetDirectories())
+    {
+        string newFilePath = Path.Combine(filePath, directoryInfo1.Name);
+        string newVectorPath = Path.Combine(vectorPath, directoryInfo1.Name);
+
+        if (System.IO.Directory.Exists(newVectorPath) == false)
+        {
+            continue;
+        }
+        if (System.IO.Directory.Exists(newFilePath) == false)
+        {
+            continue;
+        }
+
+        count = ScanAndVectorizeTextAndUpdateDatabaseFolder(
+            utility, newFilePath, newVectorPath, maxDocuments, count);
+
+        if ( count >= maxDocuments)
+        {
+            return count;
+        }
+    }
+
+    foreach (FileInfo fileInfo in directoryInfoVector.GetFiles())
+    {
+        if (fileInfo.Extension.ToLower() == ".txt")
+        {
+            string chkFilePath = Path.Combine(filePath, fileInfo.Name);
+            string chkVectorPath = Path.Combine(vectorPath, fileInfo.Name);
+
+            if (System.IO.File.Exists(chkFilePath) == false)
+            {
+                continue;
+            }
+            if (System.IO.File.Exists(chkVectorPath) == false)
+            {
+                continue;
+            }
+
+            if (!RekNNDocClutering.CommonValues.Settings.TextFile2IdDictionary.ContainsKey(chkFilePath))
+            {
+                Console.WriteLine($"Processing {fileInfo.FullName} ...");
+
+                int id = 0;
+                if (RekNNDocClutering.CommonValues.Settings.TextFile2IdDictionary.Count > 0)
+                {
+                    id = RekNNDocClutering.CommonValues.Settings.TextFile2IdDictionary.Values.Max() + 1;
+                }
+                RekNNDocClutering.CommonValues.Settings.TextFile2IdDictionary.Add(chkFilePath, id);
+                RekNNDocClutering.CommonValues.Settings.Id2TextFileDictionary.Add(id, chkFilePath);
+
+                List<string> lines = System.IO.File.ReadAllLines(chkVectorPath).ToList();
+
+                // lineNo- token & vector
+                Dictionary<int, List<(string, float[])>> vectorInfo = new();
+
+                try
+                {
+                    for (int i = 0; i < lines.Count; i += 3)
+                    {
+                        //Console.WriteLine($"Line {i}: {lines[i]}");
+                        if (int.TryParse(lines[i + 0], out int lineNo))
+                        {
+                            string token = lines[i + 1];
+                            float[] vec = lines[i + 2].Split(',').Select(s => float.Parse(s)).ToArray();
+
+                            if (vec.Length == BERT_VECTOR_SIZE)
+                            {
+                                // OK である
+                                vectorInfo.TryAdd(lineNo, new List<(string, float[])>());
+                                vectorInfo[lineNo].Add((token, vec));
+                            }
+                        }
+                    }
+
+                    foreach(int lineNo in vectorInfo.Keys)
+                    {
+                        string joinedToken = string.Join(" ", vectorInfo[lineNo].Select(t => t.Item1));
+                        Console.WriteLine($"Adding Vector {id},{lineNo}: " + joinedToken);
+
+                        float[][] vectors = vectorInfo[lineNo].Select(t => t.Item2).ToArray();
+
+                        utility.AddVectorForCurrentModel(
+                            vectors, id, lineNo,
+                            RekNNDocClutering.CommonValues.Settings.SearchMaxNumForAddVector,
+                            RekNNDocClutering.CommonValues.Settings.SimilarityThreshold);
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing {fileInfo.FullName} : {ex.Message}");
+                }
+
+                count += 1;
+
+                Console.WriteLine($"Total Vector : {(utility.GetTotalVectorCount(0) ?? -1).ToString("0")}");
+
+                if (count >= maxDocuments)
+                {
+                    return count;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Already processed {fileInfo.FullName}, skipping ...");
+            }
+        }
+    }
+
+    return count;
+}
+
+
+List<string> ReadFile(string fullPath)
+{
+    string fulltext = File.ReadAllText(fullPath);
+    List<string> splitTexts = fulltext.Split(new string[] { "\n", "\r\n", "\r" }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+    List<string> lines = new();
+    foreach (string text in splitTexts)
+    {
+        lines.AddRange(FSCLUnicodeLibrary.DivideTextBySentence(text));
+    }
+
+    return lines;
+}
---
+++ RekNNDocClutering/Settings.cs
@@ -0,0 +1,162 @@
+﻿// Copyright 2026 Fuuta System Service LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FuutaSystemSvcCommonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using System.Threading.Tasks;
+
+namespace RekNNDocClutering
+{
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(Settings))]
+    public partial class SettingsSerializerContext : JsonSerializerContext, FSCLJsonTypeInfoProvider<Settings>
+    {
+        public static JsonTypeInfo<Settings> GetTypeInfo()
+        {
+            return SettingsSerializerContext.Default.Settings;
+        }
+    }
+
+    public class Settings :FSCLJsonSerializeBase
+    {
+
+        //public string OnnxPath{ get; set; } = "your\\path\\model.onnx";
+
+        
+        //public string VocabPath { get; set; } = "your\\path\\vocab.txt";
+
+
+        //public int MaxTokenSize { get; set; } = 512;
+
+
+        public string TextFilePath { get; set; } = "your\\textfile\\rootpath";
+
+        public string VectorFilePath { get; set; } = "your\\textfile\\rootpath";
+
+
+        public string DatabasePath { get; set; } = "your\\path\\database";
+
+        ///// <summary>
+        ///// ファイル情報 / ファイル名単位で、テキストの行ごとに(テキスト,総トークン数)を保持する辞書
+        ///// </summary>
+        //[JsonIgnore]
+        //public Dictionary<string, Dictionary<int, int>> TextFileDictionary { get; set; } = new();
+
+        
+
+        //public class IdFileDictInfo
+        //{
+        //    public int[] ids { get; set; } = Array.Empty<int>();
+        //    public string[] Names { get; set; } = Array.Empty<string>();
+        //}
+
+
+        //public List<FSCLValuesSet<string, int, int>> TextFileValues
+        //{
+        //    get
+        //    {
+        //        List<FSCLValuesSet<string, int, int>> result = new();
+        //        foreach (var kvp in TextFileDictionary)
+        //        {
+        //            string filename = kvp.Key;
+        //            Dictionary<int, int> lineDict = kvp.Value;
+        //            foreach (var lineKvp in lineDict)
+        //            {
+        //                int lineNumber = lineKvp.Key;
+        //                int tokenCount = lineKvp.Value;
+        //                result.Add(new FSCLValuesSet<string, int, int>(filename, lineNumber, tokenCount));
+        //            }
+        //        }
+        //        return result;
+        //    }
+        //    set
+        //    {
+        //        TextFileDictionary.Clear();
+        //        foreach(FSCLValuesSet<string, int, int> item in value)
+        //        {
+        //            string filename = item.Value1 ?? string.Empty;
+        //            int lineNumber = item.Value2;
+        //            int tokenCount = item.Value3;
+        //            TextFileDictionary.TryAdd(filename, new Dictionary<int, int>());
+        //            TextFileDictionary[filename].TryAdd(lineNumber, tokenCount);
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// ファイル情報 / ファイル名とファイル番号の辞書
+        /// </summary>
+        [JsonIgnore]
+        public Dictionary<string, int> TextFile2IdDictionary { get; set; } = new();
+
+
+        //public IdFileDictInfo TextFile2IdValues
+        //{
+        //    //get
+        //    //{
+        //    //    IdFileDictInfo result = new();
+        //    //    result.ids = new int[TextFile2IdDictionary.Count];
+        //    //    result.Names = new string[TextFile2IdDictionary.Count];
+
+        //    //    for(int i = 0; i < TextFile2IdDictionary.Count; i++)
+        //    //    {
+        //    //        var kvp = TextFile2IdDictionary.ElementAt(i);
+        //    //        result.Names[i] = kvp.Key;
+        //    //        result.ids[i] = kvp.Value;
+        //    //    }
+
+        //    //    return result;
+        //    //}
+        //    set
+        //    {
+        //        TextFile2IdDictionary.Clear();
+        //        Id2TextFileDictionary.Clear();
+        //        int count = value.ids.Length;
+        //        for(int i = 0; i < count; i++)
+        //        {
+        //            TextFile2IdDictionary.TryAdd(value.Names[i], value.ids[i]);
+        //            Id2TextFileDictionary.TryAdd(value.ids[i], value.Names[i]);
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// ファイル情報 / ファイル名とファイル番号の辞書(逆引き)
+        /// </summary>
+        [JsonIgnore]
+        public Dictionary<int, string> Id2TextFileDictionary { get; set; } = new();
+
+
+        /// <summary>
+        /// コサイン類似度の閾値
+        /// </summary>
+        public double SimilarityThreshold { get; set; } = 0.9;
+
+        /// <summary>
+        /// 追加時の候補数
+        /// </summary>
+        public int SearchMaxNumForAddVector { get; set; } = 5;
+
+        /// <summary>
+        /// 最大スレッド数(評価用)
+        /// </summary>
+        //public int MaxThread { get; set; } = 20;
+    }
+}
